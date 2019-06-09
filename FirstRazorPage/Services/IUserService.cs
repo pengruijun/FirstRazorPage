1ab78357bcdeb4f4a41ccf6856417400007b3cd4
pengruijun <rpeng@rlsolutions.com>
2019-06-09 15:10:55 +0930

    first razor page example

---
+++ FirstRazorPage/Services/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using FirstRazorPage.Models;
+
+namespace FirstRazorPage.Services
+{
+    public interface IUserService
+    {
+        Task<List<User>> GetUsersAsync();
+        Task<User> GetUserAsync(int id);
+    }
+}
---
+++ FirstRazorPage/ViewComponents/UsersViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using FirstRazorPage.Services;
+
+namespace FirstRazorPage.ViewComponents
+{
+    public class UsersViewComponent : ViewComponent
+    {
+        private IUserService _userService;
+        public UsersViewComponent(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var users = await _userService.GetUsersAsync();
+            return View(users);
+        }
+    }
+}
